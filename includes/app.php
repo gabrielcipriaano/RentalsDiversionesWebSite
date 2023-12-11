0900f422230c934a7b38901dc4a0c24163d28f43
gabrielcipriaano <devgabrielcipriano@gmail.com>
2023-12-11 09:12:18 -0600

    INIT: Database Configuration and Library Inclusion
    This commit marks the start of the project development and includes the configuration of the database connection and the inclusion of essential libraries. Connection parameters such as the database name, user, and password have been set up. Additionally, necessary libraries such as ORM or routing controllers have been included to ensure smooth and efficient development.
    
    Changes made:
    - Initial configuration of the database connection.
    - Establishment of connection parameters such as database name, user, and password.
    - Inclusion of essential libraries for ORM and routing.

--- includes/app.php
+++ includes/app.php
@@ -1,9 +1,10 @@
 <?php 
-
+use Models\ActiveRecord;
 require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // Conect to database
-use Model\ActiveRecord;
 ActiveRecord::setDB($db);
