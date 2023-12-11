30d98a4425265bf769ef5646eae1af4469d17a8b
gabrielcipriaano <devgabrielcipriano@gmail.com>
2023-12-11 09:50:07 -0600

    FIX: Logic Correction in Database Connection
    This commit addresses a correction in the logic of connecting to the database. An issue affecting the proper initialization of the connection, which could lead to errors during database interaction, has been resolved. Adjustments were made to ensure a stable and successful connection when starting the application.
    
    Changes made:
    - Fixed the logic of connecting to the database.
    - Adjustments to ensure proper initialization of the connection.

--- includes/app.php
+++ includes/app.php
@@ -1,10 +1,12 @@
 <?php 
 use Models\ActiveRecord;
-require 'funciones.php';
-require 'database.php';
+
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+require 'funciones.php';
+require 'database.php';
 
 // Conect to database
 ActiveRecord::setDB($db);
