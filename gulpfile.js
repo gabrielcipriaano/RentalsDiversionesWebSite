2f916a33f6ef6ce3f0590bcb881abbd55269ec24
gabrielcipriaano <devgabrielcipriano@gmail.com>
2023-12-13 18:11:46 -0600

    FEATURE: Added Announcements Section with Responsive Design
    This commit signifies the addition of the announcements section with the corresponding responsive design. Elements and styles have been incorporated to ensure that the announcements section is visually appealing and functional across a variety of devices, including mobile and desktop.
    
    Changes made:
    - Added the announcements section.
    - Implementation of responsive design to ensure an optimal experience on different devices.

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ function css() {
       .pipe(sourcemaps.init())
       .pipe(sass())
       .pipe(postcss([autoprefixer(), cssnano()]))
-      // .pipe(postcss([autoprefixer()]))
+      .pipe(postcss([autoprefixer()]))
       .pipe(sourcemaps.write("."))
       .pipe(dest("public/build/css"))
   );
