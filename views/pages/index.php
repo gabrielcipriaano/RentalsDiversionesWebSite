9bb58a26a3310e01dbbf4128a6c3ef49503d685e
gabrielcipriaano <devgabrielcipriano@gmail.com>
2023-12-13 18:12:26 -0600

    FEATURE: Added Announcements Section with Responsive Design
    This commit signifies the addition of the announcements section with the corresponding responsive design. Elements and styles have been incorporated to ensure that the announcements section is visually appealing and functional across a variety of devices, including mobile and desktop.
    
    Changes made:
    - Added the announcements section.
    - Implementation of responsive design to ensure an optimal experience on different devices.

--- views/pages/index.php
+++ views/pages/index.php
@@ -3,7 +3,7 @@
         <h1 class="title">¡Haz de tu Celebración un Evento Inolvidable!</h1>
         <p class="message">
             Añade un toque único de diversión y emoción a cada momento especial, nuestra selección
-            variada garantiza que todos encuentren la diversión a su medida.
+            variada de <span>Brincolines</span> garantiza que todos encuentren la diversión a su medida.
         </p>
     </div>
 
@@ -11,3 +11,5 @@
         <a href="/contact" class="button">¡Reservar Ahora!</a>
     </div>
 </main>
+
+<?php include_once 'listAdds.php'; ?>
---
+++ views/pages/listAdds.php
@@ -0,0 +1,59 @@
+<section class="contenedor">
+    <h2>Brincolines Únicos para Eventos Inolvidables</h2>
+
+    <div class="container-adds">
+        <div class="add">
+            <picture class="add__img">
+                <source srcset="/build/img/mario.webp" type="image/webp">
+                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+            </picture>
+
+            <h3 class="add__name">
+                MarioBrinco
+            </h3>
+
+            <p class="add__description">
+                Diversión instantánea con nuestro brincolín inspirado en Mario Bros.
+                Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
+            </p>
+
+            <a class="add__button" href="/add?id=#">Más Información</a>
+        </div>
+        <div class="add">
+            <picture class="add__img">
+                <source srcset="/build/img/mario.webp" type="image/webp">
+                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+            </picture>
+
+            <h3 class="add__name">
+                MarioBrinco
+            </h3>
+
+            <p class="add__description">
+                Diversión instantánea con nuestro brincolín inspirado en Mario Bros.
+                Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
+            </p>
+
+            <a class="add__button" href="/add?id=#">Más Información</a>
+        </div>
+        <div class="add">
+            <picture class="add__img">
+                <source srcset="/build/img/mario.webp" type="image/webp">
+                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+            </picture>
+
+            <h3 class="add__name">
+                MarioBrinco
+            </h3>
+
+            <p class="add__description">
+                Diversión instantánea con nuestro brincolín inspirado en Mario Bros.
+                Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
+            </p>
+
+            <a class="add__button" href="/add?id=#">Más Información</a>
+        </div>
+
+
+    </div>
+</section>
