07dd3c94276aed456dcff91fc8b6bbedeceff69b
gabrielcipriaano <devgabrielcipriano@gmail.com>
2023-12-15 15:43:21 -0600

    UPDATE: User Interface (UI) Update in the Announcements Section
    This commit signifies an update to the user interface (UI) in the announcements section. Changes and improvements have been made to the design and visual presentation to provide a more attractive and functional experience for users interacting with the announcements section.
    
    Changes made:
    - UI update in the announcements section.
    - Changes and improvements in design for a more attractive and functional experience.

--- views/pages/listAdds.php
+++ views/pages/listAdds.php
@@ -4,25 +4,35 @@
     <div class="container-adds">
         <div class="add">
             <picture class="add__img">
-                <source srcset="/build/img/mario.webp" type="image/webp">
-                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+                <source srcset="/build/img/bob.webp" type="image/webp">
+                <img loading="lazy" src="/build/img/bob.jpg" alt="imagen bob" class="add__img-src">
             </picture>
 
             <h3 class="add__name">
-                MarioBrinco
+                BobBrinco
             </h3>
 
             <p class="add__description">
-                Diversión instantánea con nuestro brincolín inspirado en Mario Bros.
+                Diversión instantánea con nuestro brincolín inspirado en BOB.
                 Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
             </p>
 
-            <a class="add__button" href="/add?id=#">Más Información</a>
+            <a href="/add?id=#" class="add__button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-info-circle" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                    <path d="M12 9h.01" />
+                    <path d="M11 12h1v4h1" />
+                </svg>
+                <p>Más Información</p>
+
+            </a>
+
         </div>
         <div class="add">
             <picture class="add__img">
                 <source srcset="/build/img/mario.webp" type="image/webp">
-                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+                <img loading="lazy" src="/build/img/mario.jpg" alt="imagen mario" class="add__img-src">
             </picture>
 
             <h3 class="add__name">
@@ -34,12 +44,22 @@
                 Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
             </p>
 
-            <a class="add__button" href="/add?id=#">Más Información</a>
+            <a href="/add?id=#" class="add__button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-info-circle" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                    <path d="M12 9h.01" />
+                    <path d="M11 12h1v4h1" />
+                </svg>
+                <p>Más Información</p>
+
+            </a>
+
         </div>
         <div class="add">
             <picture class="add__img">
                 <source srcset="/build/img/mario.webp" type="image/webp">
-                <img loading="lazy" src="/build/img/mario.jpg" alt="Logo Rental's y Diversiones" class="add__img-src">
+                <img loading="lazy" src="/build/img/mario.jpg" alt="imagen mario" class="add__img-src">
             </picture>
 
             <h3 class="add__name">
@@ -51,9 +71,21 @@
                 Salta, ríe y celebra con un toque de aventura. ¡La fiesta comienza aquí!
             </p>
 
-            <a class="add__button" href="/add?id=#">Más Información</a>
+            <a href="/add?id=#" class="add__button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-info-circle" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                    <path d="M12 9h.01" />
+                    <path d="M11 12h1v4h1" />
+                </svg>
+                <p>Más Información</p>
+
+            </a>
+
         </div>
+    </div>
 
-
+    <div class="right">
+        <a href="/adds" class="botton"> Ver Todos</a>
     </div>
 </section>
