3a8113c52a0351527f3dfb5ca321d77c4d670294
gabrielcipriaano <devgabrielcipriano@gmail.com>
2024-01-04 19:01:44 -0600

    ENHANCEMENT: Configuration of Real Data and Environment Variables for SMTP Email Sending
    This commit signifies the improvement made by configuring real data for sending emails via SMTP. Necessary environment variables have been set for the secure and functional configuration of the application when sending emails.
    
    Changes made:
    - Configuration of real data for sending emails via SMTP.
    - Establishment of environment variables for secure and functional configuration.
    
    These configurations enable proper authentication and email sending via SMTP, using real data for comprehensive functionality. The commit is ready for review and integration into the main branch.

--- classes/Email.php
+++ classes/Email.php
@@ -16,27 +16,27 @@ class Email
         $this->email = $email ?? '';
         $this->token = $token ?? '';
     }
-   
+
 
     public function sendResetInstructions()
     {
         $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
-        $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+        $phpmailer->Host = $_ENV['MAIL_HOST'];
         $phpmailer->SMTPAuth = true;
-        $phpmailer->Port = 2525;
-        $phpmailer->Username = '9daf845769fc4c';
-        $phpmailer->Password = '5efe2d93194520';
+        $phpmailer->Port = $_ENV['MAIL_PORT'];
+        $phpmailer->Username = $_ENV['MAIL_USER'];
+        $phpmailer->Password = $_ENV['MAIL_PASSWORD'];
 
-        $phpmailer->setFrom("Rental's Y Diversiones.com","Rental's Y Diversiones");
-        $phpmailer->addAddress($this->email,"Rental's Y Diversiones.com");
+        $phpmailer->setFrom($_ENV['MAIL_USER'], "Rental's Y Diversiones");
+        $phpmailer->addAddress($this->email, "Rental's Y Diversiones");
 
-        $phpmailer->isHTML(true); 
+        $phpmailer->isHTML(true);
         $phpmailer->CharSet = 'UTF-8';
         $phpmailer->Subject = "Restablece tu password Rental's Y Diversiones";
-        $confirmationLink = 'http://192.168.100.9:5000/reset?token=' . $this->token;
+        $confirmationLink = $_ENV['SERVER_HOST'] .'/reset?token=' . $this->token;
 
-        
+
         $phpmailer->Body = "
             <div style='font-family: Arial, sans-serif; color: #333;'>
                 <div style='background-color: #004643; padding: 20px; color: #fff; text-align: center;'>
@@ -70,31 +70,31 @@ class Email
             </div>
         ";
 
-        
 
-        $phpmailer->send();   
+
+        $phpmailer->send();
     }
 
     public function sendContact()
-{
-    $answer  = $_POST['contact'];
+    {
+        $answer  = $_POST['contact'];
 
-    $phpmailer = new PHPMailer();
-    $phpmailer->isSMTP();
-    $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-    $phpmailer->SMTPAuth = true;
-    $phpmailer->Port = 2525;
-    $phpmailer->Username = '9daf845769fc4c';
-    $phpmailer->Password = '5efe2d93194520';
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $_ENV['MAIL_HOST'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $_ENV['MAIL_PORT'];
+        $phpmailer->Username = $_ENV['MAIL_USER'];
+        $phpmailer->Password = $_ENV['MAIL_PASSWORD'];
 
-    $phpmailer->setFrom("rentalsydiversiones@gmail.com","Rental's Y Diversiones");
-    $phpmailer->addAddress('email@email.com',"Rental's Y Diversiones.com");
+        $phpmailer->setFrom($_ENV['MAIL_USER'], "Rental's Y Diversiones");
+        $phpmailer->addAddress('rentalsydiversiones@gmail.com', "Rental's Y Diversiones");
 
-    $phpmailer->isHTML(true);
-    $phpmailer->CharSet = 'UTF-8';
-    $phpmailer->Subject = 'Tienes un nuevo mensaje';
+        $phpmailer->isHTML(true);
+        $phpmailer->CharSet = 'UTF-8';
+        $phpmailer->Subject = 'Tienes un nuevo mensaje';
 
-    $phpmailer->Body = "
+        $phpmailer->Body = "
         <div style='font-family: Arial, sans-serif; color: #333;'>
             <div style='background-color: #004643; padding: 20px; color: #fff; text-align: center;'>
                 <h2 style='margin: 0;'>Rental's Y Diversiones</h2>
@@ -106,19 +106,19 @@ class Email
                 <p> Nombre: {$answer['name']}</p>
                 <p> Mensaje: {$answer['message']}</p>";
 
-    if ($answer['type-contact'] == 'email') {
-        $phpmailer->Body .= "
+        if ($answer['type-contact'] == 'email') {
+            $phpmailer->Body .= "
                 <p> El usuario prefiere ser contactado por: {$answer['type-contact']}</p>
                 <p> Email: {$answer['email']}</p>";
-    } else {
-        $phpmailer->Body .= "
+        } else {
+            $phpmailer->Body .= "
                 <p> El usuario prefiere ser contactado por: {$answer['type-contact']}</p>
                 <p> Telefono: {$answer['phone']}</p>
                 <p> Fecha: {$answer['date']}</p>
                 <p> Hora: {$answer['time']}</p>";
-    }
+        }
 
-    $phpmailer->Body .= "
+        $phpmailer->Body .= "
             </div>
             <div style='background-color: #004643; color: #fff; text-align: center; padding: 10px;'>
                 <p style='margin: 0;'>Atentamente,<br>Equipo de Rental's Y Diversiones</p>
@@ -126,13 +126,12 @@ class Email
         </div>
     ";
 
-    if ($phpmailer->send()) {
-        $message = '<div class="alert success w-100"> Mensaje enviado </div>';
-    } else {
-        $message = '<div class="alerta error w-100"> No se pudo enviar el mensaje </div>';
+        if ($phpmailer->send()) {
+            $message = '<div class="alert success w-100"> Mensaje enviado </div>';
+        } else {
+            $message = '<div class="alerta error w-100"> No se pudo enviar el mensaje </div>';
+        }
+
+        return $message;
     }
-
-    return $message;
-}
-
 }
